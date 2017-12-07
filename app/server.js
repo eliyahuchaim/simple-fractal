c6dd0f167e21c205f7b12bb7e212983978ceb6b9
github username <github email address>
2017-12-06 22:57:37 -0500

    score component

--- app/server.js
+++ app/server.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const port = 8000;
 
 const app = express();
 
 
-const requestF = function() {
-  var request = require("request");
-
-  var options = { method: 'GET',
-    url: 'https://s3.amazonaws.com/simple-fractal-recruiting/companies.csv'};
-
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-    // console.log(body);
-    return body
-  });
-}
+// const requestF = function() {
+//   var request = require("request");
+//
+//   var options = { method: 'GET',
+//     url: 'https://s3.amazonaws.com/simple-fractal-recruiting/companies.csv'};
+//
+//   request(options, function (error, response, body) {
+//     if (error) throw new Error(error);
+//     // console.log(body);
+//     return body
+//   });
+// }
 
 // app.get('/', (req, res) => res.send(requestF()))
 
